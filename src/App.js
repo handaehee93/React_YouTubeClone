5f141b954e69355c95995980918299359a4afe6d
handaehee93 <hdh8990@naver.com>
2023-02-24 17:03:23 +0900

    SearchHeader 완성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import SearchHeader from './components/SearchHeader/SearchHeader';
 
 
 
 function App() {
   return (
     <>
+      <SearchHeader />
       <Outlet />
     </>
   );
