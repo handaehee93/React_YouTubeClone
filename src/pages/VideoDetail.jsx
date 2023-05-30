b96ed833b2b5a7154854bffad8e4858269557363
handaehee93 <hdh8990@naver.com>
2023-05-30 17:02:40 +0900

    완성

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -14,7 +14,7 @@ export default function VideoDetail() {
           type="text/html" 
           width="100%" 
           height="640"
-          src={`http://www.youtube.com/embed/${video.id}`}
+          src={`https://www.youtube.com/embed/${video.id}`}
           frameBorder="0" 
           title={title}
         />
